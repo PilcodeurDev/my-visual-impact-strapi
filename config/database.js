1e9426aa8e70049092df8618b04c00c8fcc1abea
simon despres <simondprs62790@hotmail.fr>
2023-10-30 10:48:42 +0100

    setup strapi with DB Postgresql

--- config/database.js
+++ config/database.js
@@ -4,14 +4,12 @@
         client: 'postgres',
         connection: {
           host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5433),
-          database: env('DATABASE_NAME', 'strapi-api'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'test'),
           user: env('DATABASE_USERNAME', 'postgres'),
           password: env('DATABASE_PASSWORD', 'Dofus62790!'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
+          ssl: env('DATABASE_SSl', false),
         },
         debug: false,
       },
